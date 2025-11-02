344eb157c27c50541c453daa8775fc6750cecb60
Shaikh Firoz <firozshaikh13@outlook.com>
2025-11-02 17:12:59 +0530

    changed the index

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask,send_file,  jsonify     #render_template
 import pyjokes
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return send_file('index.html')
 
 @app.route('/get_joke')
 def get_joke():
